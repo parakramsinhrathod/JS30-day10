fbcd46cd860e1bff11fe8ca8b26160769b2e45af
Vishwajeetsinh <rathodvishwajeet64@gmail.com>
2023-02-19 20:58:06 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+let lastChecked
+function handleCheck(e){
+    // check if they had shift key down
+    // AND check that they are checking it
+    let inBetween  = false
+    if(e.shiftKey && this.checked){
+        // go ahead and do what we need
+        // loop over every single checkbox
+        checkboxes.forEach(checkbox  => {
+            console.log(checkbox)
+            if(checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween
+                console.log('staring to check them between!')
+            }
+            if(inBetween){
+                checkbox.checked = true
+            }
+        })
+    }
+    lastChecked = this
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
